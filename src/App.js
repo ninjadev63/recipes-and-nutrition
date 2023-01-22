935c68510915479858c7cdc08981a3479cde436a
julia_topolska <topolsk92@gmail.com>
2023-01-23 00:16:11 +0200

    add all project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Nutrition from './componentsBtnTwo/Nutrition';
+import Recipes from './componentsBtnOne/Recipes';
+import video from './food.mp4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+
+      <header className='logo'>
+        <video autoPlay muted loop playsInline>
+          <source src={ video } type='video/mp4'/>
+        </video>
+          <Link to='/' className='brand'>Yummy&Healthy</Link>
       </header>
-    </div>
+
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/recipes' element={<Recipes />}></Route>
+        <Route path='/nutrition' element={<Nutrition />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
